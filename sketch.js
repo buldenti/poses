30542406e7a10489747000439a0a8382afd03a07
buldenti <59711896+buldenti@users.noreply.github.com>
2024-02-12 18:51:19 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,67 +5,54 @@
 
 /* ===
 ml5 Example
-PoseNet using p5.js
+PoseNet example using p5.js
 === */
-/* eslint-disable */
 
-// Grab elements, create settings, etc.
-var video = document.getElementById("video");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-// The detected positions will be inside an array
+let video;
+let poseNet;
 let poses = [];
 
-// Create a webcam capture
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    video.srcObject = stream;
-    video.play();
+function setup() {
+  createCanvas(windowWidth, windowHeight;
+  video = createCapture(VIDEO);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
   });
-}
-
-// A function to draw the video and poses into the canvas.
-// This function is independent of the result of posenet
-// This way the video will not seem slow if poseNet
-// is not detecting a position
-function drawCameraIntoCanvas() {
-  // Draw the video element into the canvas
-  ctx.drawImage(video, 0, 0, 640, 480);
-  // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
-  window.requestAnimationFrame(drawCameraIntoCanvas);
-}
-// Loop over the drawCameraIntoCanvas function
-drawCameraIntoCanvas();
-
-// Create a new poseNet method with a single detection
-const poseNet = ml5.poseNet(video, modelReady);
-poseNet.on("pose", gotPoses);
-
-// A function that gets called every time there's an update from the model
-function gotPoses(results) {
-  poses = results;
+  // Hide the video element, and just show the canvas
+  video.hide();
 }
 
 function modelReady() {
-  console.log("model ready");
-  poseNet.multiPose(video);
+  select('#status').html('Model Loaded');
+}
+
+function draw() {
+  image(video, 0, 0, video.width, video.width * video.width / video.height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
 }
 
 // A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
+function drawKeypoints()  {
   // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
+  for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
-      let keypoint = poses[i].pose.keypoints[j];
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        ctx.beginPath();
-        ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
     }
   }
@@ -74,15 +61,20 @@ function drawKeypoints() {
 // A function to draw the skeletons
 function drawSkeleton() {
   // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i += 1) {
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
     // For every skeleton, loop through all body connections
-    for (let j = 0; j < poses[i].skeleton.length; j += 1) {
-      let partA = poses[i].skeleton[j][0];
-      let partB = poses[i].skeleton[j][1];
-      ctx.beginPath();
-      ctx.moveTo(partA.position.x, partA.position.y);
-      ctx.lineTo(partB.position.x, partB.position.y);
-      ctx.stroke();
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
     }
   }
+}
+
+// Resize the canvas when the
+// browser's size changes.
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
